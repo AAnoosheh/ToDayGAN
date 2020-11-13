223653de7c6563897f406efc63b3da03599b0876
Daniel <pollithy.daniel@gmail.com>
2020-11-13 09:42:57 +0100

    Set training epochs in train_combogan.sh
    For better reproducibility. See https://github.com/AAnoosheh/ToDayGAN/issues/20

--- scripts/train_combogan.sh
+++ scripts/train_combogan.sh
@@ -2,5 +2,5 @@ python train.py  \
     --dataroot ./datasets/robotcar  \
     --name robotcar_night2day  \
     --n_domains 2  \
-    --niter 25  --niter_decay 25  \
+    --niter 75  --niter_decay 75  \
     --loadSize 512  --fineSize 384
